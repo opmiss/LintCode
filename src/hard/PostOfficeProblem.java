cb8b5628e151d5e9eda4973eaa1cfc9f7c14fcd1
opmiss <opmiss@gmail.com>
2015-07-11 20:18:03 -0700

    post office problem

--- src/hard/PostOfficeProblem.java
+++ src/hard/PostOfficeProblem.java
@@ -31,62 +31,59 @@ public class PostOfficeProblem {
         if (n<=k) return 0; 
         if (k==0) return Integer.MAX_VALUE; 
         Arrays.sort(A); 
-        Element[][] dp = new Element[n+1][k+1]; 
-        for (int i=0; i<=n; i++){
-            for (int j=0; j<=i&&j<=k; j++){
-                if (i==0) {
-                    dp[i][j] = new Element(0, -1); 
-                    continue; 
-                }
-                if (j==0){
-                    dp[i][j] = new Element(Integer.MAX_VALUE, -1); 
-                    continue; 
-                }
-                if (i==j){
-                    dp[i][j] = new Element(0, i-1);
-                    continue; 
-                }
-                Element in = dp[i-1][j-1]; 
-                Element out = dp[i-1][j];
-                int dis_in, dis_out; 
-                if (in.id<0){ // j==1
-                    dis_in=0; 
-                    for (int h=0; h<i-1; h++){
-                        dis_in+=A[i-1]-A[h]; 
-                    }
-                }
-                else {
-                    dis_in = in.dis; 
-                    for (int h=i-2; h>in.id; h--){
-                        int right = A[i-1]-A[h]; 
-                        int left = A[h]-A[in.id]; 
-                        if (right > left) break; 
-                        dis_in -= (left-right); 
-                    }
-                }
-                dis_out = out.dis+A[i-1]-A[out.id];
-                if (dis_in>dis_out){
-                    dp[i][j] = new Element(dis_out, out.id); 
-                }
-                else {
-                    dp[i][j] = new Element(dis_in, i-1); 
-                }
-            }
+        Element[][] dp = new Element[k+1][n+1];
+        for (int i=0; i<=k; i++){
+        	for (int j=0; j<=n; j++){
+        		System.out.println(i+", "+j);
+        		if (i>=j) {
+        			dp[i][j] = new Element(0, i-1);
+        			continue; 
+        		}
+        		if (i==0){
+        			if (j<n) {
+        				int dis = dp[i][j-1].dis+(A[j]-A[j-1])*j;
+        				dp[i][j] = new Element(dis, -1); 
+        			}
+        			else {
+        				dp[i][j] = new Element(Integer.MAX_VALUE, -1);  
+        			}
+        			continue; 
+        		}
+        		int pid = dp[i][j-1].id; 
+        		int dis = Integer.MAX_VALUE;
+        		int last = pid; 
+        		for (int start = pid; start<j; start++){ 
+        			int td = dp[i-1][start].dis+calc(start, j, A); 
+        			if (dis>=td) {
+        				dis = td;
+        				last = start; 
+        			}
+        		}
+        		dp[i][j] = new Element(dis, last); 	
+        	}
         }
-        //print out elements 
-        for (int i=0; i<=n; i++){
-            for (int j=0; j<=i&&j<=k; j++){
-            	System.out.print("("+dp[i][j].dis+", "+dp[i][j].id+")");
-            }
-            System.out.println(); 
-        }
-        return dp[n][k].dis; 
+        return dp[k][n].dis; 
     }
     
+    public int calc(int start, int end, int[] A){
+    	int dis = 0;
+    	if (end == A.length){
+    		for (int i=start+1; i<end; i++){
+    			dis+=A[i]-A[start]; 
+    		}
+    		return dis; 
+    	}
+    	for (int i=start+1; i<end; i++ ){
+    		int left = A[i]-A[start]; 
+    		int right = A[end] - A[i]; 
+    		dis+=(left<right)?left:right; 
+    	}
+    	return dis; 
+    }
     
     public static void main(String[] args){
     	int[] A = new int[]{1,3,5,7,10}; 
     	PostOfficeProblem pop = new PostOfficeProblem(); 
-    	System.out.println(pop.postOffice(A, 1)); 
+    	System.out.println(pop.postOffice(A, 3)); 
     }
 }
